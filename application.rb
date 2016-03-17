8e67309c905d17342b9c3a6cac21fc96d2226942
T-Dnzt <thi.denizet@gmail.com>
2016-03-17 11:18:27 +0700

    Remove support for application/json

--- application.rb
+++ application.rb
@@ -15,16 +15,31 @@ Dir["#{File.dirname(__FILE__)}/app/api/**/*.rb"].each { |f| require f }
 Dir["#{File.dirname(__FILE__)}/app/yumi/**/*.rb"].each { |f| require f }
 Dir["#{File.dirname(__FILE__)}/app/presenters/**/*.rb"].each {|f| require f}
 
-# Grape API class. We will inherit from it in our future controllers.
+# Grape API class. We inherit from it in our controllers.
 module API
   class Root < Grape::API
-    format :json
     prefix :api
+
+    format :json
+    formatter :json, -> (object, _env) { object.to_json }
+    content_type :json, 'application/vnd.api+json'
 
     helpers do
       def base_url
         "http://#{request.host}:#{request.port}/api/#{version}"
       end
+
+      def invalid_media_type!
+        error!('Unsupported media type', 415)
+      end
+
+      def json_api?
+        request.content_type == 'application/vnd.api+json'
+      end
+    end
+
+    before do
+      invalid_media_type! unless json_api?
     end
 
     # Simple endpoint to get the current status of our API.
